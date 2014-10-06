c7ac3c0d0e9ab387162c60b39fd811894dc02855
imduffy15 <ian@ianduffy.ie>
2014-10-06 15:24:00 +0100

    update git ignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,112 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+'use strict';
 
-var paths = {
-  sass: ['./scss/**/*.scss']
+var gulp = require('gulp'),
+    prefix = require('gulp-autoprefixer'),
+    minifyCss = require('gulp-minify-css'),
+    usemin = require('gulp-usemin'),
+    uglify = require('gulp-uglify'),
+    minifyHtml = require('gulp-minify-html'),
+    livereload = require('gulp-livereload'),
+    imagemin = require('gulp-imagemin'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    jshint = require('gulp-jshint'),
+    rev = require('gulp-rev'),
+    connect = require('gulp-connect'),
+    proxy = require('proxy-middleware'),
+    es = require('event-stream'),
+    flatten = require('gulp-flatten'),
+    clean = require('gulp-clean'),
+    replace = require('gulp-replace'),
+    browserify = require('gulp-browserify');
+
+var yeoman = {
+    app: 'src/',
+    dist: 'www/',
+    tmp: '.tmp/'
 };
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+gulp.task('clean', function () {
+    return gulp.src(yeoman.dist, {read: false}).
+        pipe(clean());
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+gulp.task('clean:tmp', function () {
+    return gulp.src(yeoman.tmp, {read: false}).
+        pipe(clean());
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+gulp.task('copy', ['clean'], function () {
+    return es.merge(gulp.src(yeoman.app + 'i18n/**').
+            pipe(gulp.dest(yeoman.dist + 'i18n/')),
+        gulp.src(yeoman.app + '**/*.{woff,svg,ttf,eot}').
+            pipe(flatten()).
+            pipe(gulp.dest(yeoman.dist + 'fonts/')));
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+gulp.task('images', function () {
+    return gulp.src(yeoman.app + 'img/**').
+        pipe(imagemin({optimizationLevel: 5})).
+        pipe(gulp.dest(yeoman.dist + 'img'));
+});
+
+
+gulp.task('styles', [], function () {
+    return gulp.src(yeoman.app + '{,**/**}*.css').
+        pipe(gulp.dest(yeoman.tmp));
+});
+
+gulp.task('server', ['watch'], function () {
+    connect.server(
+        {
+            root: [yeoman.app, yeoman.tmp],
+            port: 9000,
+            livereload: true
+        }
     );
-    process.exit(1);
-  }
-  done();
+});
+
+gulp.task('watch', function () {
+    gulp.watch(yeoman.app + 'scripts/**', ['browserify']);
+    gulp.watch(yeoman.app + 'img/**', ['images']);
+    livereload();
+});
+
+gulp.task('server:dist', ['build'], function () {
+    connect.server(
+        {
+            root: [yeoman.dist],
+            port: 9000
+        }
+    );
+});
+
+gulp.task('build', ['clean', 'copy'], function () {
+    gulp.run('usemin');
+});
+
+gulp.task('usemin', ['images', 'styles'], function () {
+    return gulp.src(yeoman.app + '{,templates/}*.html').
+        pipe(usemin({
+            css: [
+                prefix.apply(),
+                replace(/[0-9a-zA-Z\-_\s\.\/]*\/([a-zA-Z\-_\.0-9]*\.(woff|eot|ttf|svg))/g, '/fonts/$1'),
+                //minifyCss(),
+                'concat',
+                rev()
+            ],
+            html: [
+                minifyHtml({empty: true, conditionals: true})
+            ],
+            js: [
+                ngAnnotate(),
+                uglify(),
+                'concat',
+                rev()
+            ]
+        })).
+        pipe(gulp.dest(yeoman.dist));
+});
+
+gulp.task('default', function () {
+    gulp.run('build');
 });
