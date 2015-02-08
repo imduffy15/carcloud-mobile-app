b2d22e3efac25fa0ef60a5652596b33d8193cdc6
Ian Duffy <ian@ianduffy.ie>
2015-02-08 19:16:05 +0000

    bring in google maps

--- src/js/constants.js
+++ src/js/constants.js
@@ -9,7 +9,7 @@ carcloudApp.constant('USER_ROLES', {
 });
 
 carcloudApp.constant('API_DETAILS', {
-    baseUrl: 'http://carcloud.ianduffy.ie/',
+    baseUrl: 'http://localhost:8080/',
     clientId: 'carcloudapp',
     clientSecret: 'Echoong7zooNga3tvohy6Xaeoon9Aem3ange8Iga5ooDa1ahb8LaS2',
     grantType: 'password',
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -62,9 +62,97 @@ carcloudApp.controller('DeviceCtrl', function ($scope, $ionicModal, Device, User
 
 });
 
-carcloudApp.controller('DeviceSingleCtrl', function ($scope, $ionicPopover, $ionicModal, $state, Device, device) {
+carcloudApp.controller('DeviceSingleCtrl', function ($scope, $ionicPopover, $ionicModal, $state, $filter, Device, device) {
 
     $scope.device = device;
+
+
+    $scope.onChangeDate = function () {
+        var dateFormat = 'yyyy-MM-dd';
+        var fromDate = $filter('date')($scope.fromDate, dateFormat);
+        var toDate = $filter('date')($scope.toDate, dateFormat);
+
+        device.resource("tracks").query({
+            'fromDate': '2014-12-12',
+            'toDate': '2015-12-12'
+        }).$promise.then(function (tracks) {
+                device.tracks = tracks;
+                addMarkers();
+            });
+    };
+
+$scope.onChangeDate();
+
+    var addMarkers = function () {
+
+        var polyLineCoordinates = [];
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(53, -8)
+        };
+
+        $scope.markers = [];
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        var infoWindow = new google.maps.InfoWindow({maxWidth: 350});
+
+        if (device.tracks.length > 0) {
+
+            var createMarker = function (track) {
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    position: new google.maps.LatLng(track.latitude, track.longitude),
+                    title: 'Track ' + track.id
+                });
+                polyLineCoordinates.push(marker.position);
+
+                marker.content = '<div class="infoWindowContent">';
+
+                marker.content = marker.content + '<p>longitude: ' + track.longitude + '</p>';
+                marker.content = marker.content + '<p>latitude: ' + track.latitude + '</p>';
+                marker.content = marker.content + '<p>Recorded at: ' + new Date(track.recordedAt) + '</p>';
+
+
+                if (track.fields.length > 0) {
+                    marker.content = marker.content + '<table><tr><th>Name</th><th>Value</th></tr>';
+
+                    angular.forEach(track.fields, function (field) {
+                        marker.content = marker.content + '<tr><td>' + field.name + '</td><td>' + field.value + '</td></tr>';
+                    });
+                }
+
+                marker.content = marker.content + '</table></div>';
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                    infoWindow.open($scope.map, marker);
+                });
+
+                $scope.markers.push(marker);
+
+            };
+
+            angular.forEach(device.tracks, function (track) {
+                createMarker(track);
+            });
+
+            $scope.map.setCenter($scope.markers[Math.round(($scope.markers.length - 1) / 2)].position);
+
+            var path = new google.maps.Polyline({
+                path: polyLineCoordinates
+            });
+            path.setMap($scope.map);
+        }
+
+
+    };
+
+    addMarkers();
+
+    $scope.openInfoWindow = function (e, selectedMarker) {
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
+
 
     $ionicPopover.fromTemplateUrl('templates/device-popover.html', {
         scope: $scope
