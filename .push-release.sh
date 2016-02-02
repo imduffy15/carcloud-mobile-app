edcc9a75aac92218ae13280a6e21b3bd3e16b916
Noor Mohamed <aknoormohamed@gmail.com>
2016-02-02 12:53:10 +0530

    Update .push-release.sh

--- .push-release.sh
+++ .push-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git config --global user.name "Travis-CI"
-git config --global user.email "travisci@ianduffy.ie"
-git remote set-url origin https://${GH_TOKEN}@github.com/imduffy15/carcloud-mobile-app.git
+git config --global user.name "Noor"
+git config --global user.email "aknoormohamed@gmail.com"
+git remote set-url origin https://${GH_TOKEN}@github.com/aknoormohamed/carcloud-mobile-app.git
 git fetch --tags
 
 export VERSION=`grep -Po '(?<="version": ")[^"]*' package.json`
@@ -10,4 +10,4 @@ export VERSION=`grep -Po '(?<="version": ")[^"]*' package.json`
 if [ -z "${TRAVIS_TAG}" ]; then git tag $VERSION; fi
 if [ -z "${TRAVIS_TAG}" ]; then git push --tags; fi
 
-if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/imduffy15/carcloud-mobile-app.git; git add .; git commit -a -m "init"; git push -u origin -f master:gh-pages; cd ../; fi
+if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/aknoormohamed/carcloud-mobile-app.git; git add .; git commit -a -m "init"; git push -u origin -f master:gh-pages; cd ../; fi
